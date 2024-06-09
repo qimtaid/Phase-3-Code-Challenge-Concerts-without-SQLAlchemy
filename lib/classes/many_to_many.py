1c17bb29ba81313126bab1873d9fde6e54f007fb
Dan Koskei <qimtaid@gmail.com>
2024-06-09 23:55:06 +0300

    Final commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,19 +1,46 @@
+# lib/models/many_to_many.py
+
 class Band:
+    all_bands = []
+
     def __init__(self, name, hometown):
-        self.name = name
-        self.hometown = hometown
+        self._name = name
+        self._hometown = hometown
+        self._concerts = []
+        Band.all_bands.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    @property
+    def hometown(self):
+        return self._hometown
+
+    @hometown.setter
+    def hometown(self, value):
+        raise AttributeError("Hometown is immutable")
 
     def concerts(self):
-        pass
+        return self._concerts
 
     def venues(self):
-        pass
+        return list(set(concert.venue for concert in self._concerts))
 
     def play_in_venue(self, venue, date):
-        pass
+        concert = Concert(date=date, band=self, venue=venue)
+        self._concerts.append(concert)
+        return concert
 
     def all_introductions(self):
-        pass
+        return [f"Hello {concert.venue.city}!!!!! We are {self.name} and we're from {self.hometown}" for concert in self._concerts]
 
 
 class Concert:
@@ -21,21 +48,49 @@ class Concert:
         self.date = date
         self.band = band
         self.venue = venue
-
-    def hometown_show(self):
-        pass
-
-    def introduction(self):
-        pass
+        band.concerts().append(self)
+        venue.concerts().append(self)
 
 
 class Venue:
+    all_venues = []
+
     def __init__(self, name, city):
-        self.name = name
-        self.city = city
+        self._name = name
+        self._city = city
+        self._concerts = []
+        Venue.all_venues.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._city = value
+        else:
+            raise ValueError("City must be a non-empty string")
 
     def concerts(self):
-        pass
+        return self._concerts
 
     def bands(self):
-        pass
+        return list(set(concert.band for concert in self._concerts))
+
+    def concert_on(self, date):
+        for concert in self._concerts:
+            if concert.date == date:
+                return concert
+        return None
